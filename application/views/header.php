c344da3ea92d42bfffacc75c38ba233e5680b346
wael-Fadlallah <wael.fadlallah@gmail.com>
2020-11-07 16:26:54 +0700

    add assets

--- application/views/header.php
+++ application/views/header.php
@@ -5,14 +5,13 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <link rel='stylesheet' href='<?php echo base_url() ; ?>/style/bootstrap.css'>
     <link rel='stylesheet' href='<?php echo base_url() ; ?>/assets/css/style-starter.css'>
-    <link rel='stylesheet' href='<?php echo base_url() ; ?>/style/style4.css'>
+    <link rel='stylesheet' href='<?php echo base_url() ; ?>/style/style.css'>
     <script src='<?php echo base_url() ; ?>/js/jquery.js'></script>
     <script src='<?php echo base_url() ; ?>/js/bootstrap.js'></script>
     <script src='<?php echo base_url() ; ?>/js/popper.js'></script>
     <script src='<?php echo base_url() ; ?>/assets/js/jquery.magnific-popup.min.js' defer></script>
     <script src='<?php echo base_url() ; ?>/assets/js/owl.carousel.js'></script>
-    <link href="//fonts.googleapis.com/css?family=Roboto:300,400,500,700,900&display=swap" rel="stylesheet">
-    <link href="//fonts.googleapis.com/css?family=Nunito:200,300,400,600,700,800,900&display=swap" rel="stylesheet">
+    <link href="<?php echo base_url() ; ?>/assets/fonts/Tajawal-Regular.ttf" rel="stylesheet">
 	<meta charset="utf-8">
         <title><?php echo $title; ?></title>
         <div style="display:none" id="usersession"><?php echo $this->session->userdata('user');?></div>
